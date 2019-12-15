3d3b58c6e7df9504fd30cc07521aa3b53cf7796a
bmmtstb <bmmartint.stb@web.de>
2019-12-15 01:02:17 +0100

    renamed input to puzzle_input, to make it more consistent
    Day03.py 1 finished

--- 2019/Day02.py
+++ 2019/Day02.py
@@ -7,7 +7,7 @@ import unittest
 
 from parameterized import parameterized
 
-global_intcode = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 19, 6, 23, 2, 23, 13, 27, 1, 27, 5,
+puzzle_intcode = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 19, 6, 23, 2, 23, 13, 27, 1, 27, 5,
                   31, 2, 31, 10, 35, 1, 9, 35, 39, 1, 39, 9, 43, 2, 9, 43, 47, 1, 5, 47, 51, 2, 13, 51, 55, 1, 55, 9, 59,
                   2, 6, 59, 63, 1, 63, 5, 67, 1, 10, 67, 71, 1, 71, 10, 75, 2, 75, 13, 79, 2, 79, 13, 83, 1, 5, 83, 87,
                   1, 87, 6, 91, 2, 91, 13, 95, 1, 5, 95, 99, 1, 99, 2, 103, 1, 103, 6, 0, 99, 2, 14, 0, 0]
@@ -88,7 +88,7 @@ if __name__ == '__main__':
     # make sure to use unique memory for every test
     # before running the program, replace position 1 with the value 12 and replace position 2 with the value 2.
     # What value is left at position 0 after the program halts?
-    memory1 = global_intcode.copy()
+    memory1 = puzzle_intcode.copy()
     memory1[1] = 12  # noun
     memory1[2] = 2  # verb
     result1, output1 = run_intcode_program(memory1)
@@ -96,6 +96,6 @@ if __name__ == '__main__':
     print("Pos 0: ", output1)
 
     # find a pair of inputs that produce output 19690720
-    memory2 = global_intcode.copy()
+    memory2 = puzzle_intcode.copy()
     result2 = find_intcode_inputs(memory2, [1, 2], [(0, 99), (0, 99)], 19690720)
     print("100 * noun + verb: ", 100 * result2[0] + result2[1])
