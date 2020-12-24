9344d02a07ba8fb581d269297d1c2b1e77190486
Martin <bmmartint.stb@web.de>
2020-12-24 01:48:41 +0100

    added day 25 and allowed tests for parallel run

--- Run_config_creator.py
+++ Run_config_creator.py
@@ -5,11 +5,11 @@ years = [2019, 2020]
 dir_base = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 run_base = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".idea/runConfigurations")
 for year in years:
-    for day_num in range(1, 25):
+    for day_num in range(1, 26):
         day = "0" + str(day_num) if day_num < 10 else str(day_num)
         text = \
             '<component name="ProjectRunConfigurationManager">\
-              <configuration default="false" name="{year}-Day{day}" type="PythonConfigurationType" factoryName="Python" folderName="{year}">\
+              <configuration default="false" name="{year}-Day{day}" type="PythonConfigurationType" factoryName="Python" folderName="{year}"  singleton="false">\
                 <module name="adventofcode" />\
                 <option name="INTERPRETER_OPTIONS" value="" />\
                 <option name="PARENT_ENVS" value="true" />\
