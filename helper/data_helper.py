b9fa2a45430f774278b3025e562ccc57a5c018ab
Martin <martin@fam-steinborn.de>
2021-12-02 12:57:37 +0100

    added split operation to data linereader

--- helper/data_helper.py
+++ helper/data_helper.py
@@ -1,7 +1,12 @@
-def read_lines_as_list(filepath, t: type = str):
+def read_lines_as_list(filepath, t: type = str, split: str = None):
     """reads the file at filepath"""
     data = []
+    if split and t != str:
+        raise Exception("Bad parameters")
     with open(filepath) as file:
         for line in file.readlines():
-            data.append(t(line.replace("\n", "")))
+            if split:
+                data.append(line.replace("\n", "").split(split))
+            else:
+                data.append(t(line.replace("\n", "")))
     return data
