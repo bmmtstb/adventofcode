2d7be905d550b7cfb09914379d9f5e338a4863df
Martin <bmmartint.stb@web.de>
2021-12-05 14:50:48 +0100

    2021 - Day 04

--- 2019/Day15.py
+++ 2019/Day15.py
@@ -1,5 +1,58 @@
+import unittest
+from parameterized import parameterized
+from typing import Dict, List, Tuple, Set
+
+from helper.file import read_lines_as_list
+from helper.tuple import tuple_add_tuple
+from Day05 import run_intcode_program
+
+
+# Directions
+# north (1), south (2), west (3), and east (4)
+dirs = {
+    1: (0,  1),
+    2: (0, -1),
+    3: (-1, 0),
+    4: (1,  0)
+}
+
+
+def find_shortest_path(
+        program: List[int],
+        valid_programs: List[List[int]]
+) -> (int, List[int]):
+    """find the length of the shortest path by generating all paths with length n"""
+    new_valid_programs = []
+    for valid in valid_programs:
+        for curr_dir in dirs.keys():
+            new_input = valid + [curr_dir]
+            outp, _, _, _ = run_intcode_program(program, new_input)
+            # Status (output[0])
+            # 0: The repair droid hit a wall. Its position has not changed.
+            # 1: The repair droid has moved one step in the requested direction.
+            # 2: The repair droid has moved one step in the requested direction; new pos == oxygen system pos.
+            if outp:  # 1 or 2
+                # add new found program to possible descendants
+                new_valid_programs.append(new_input)
+                if outp == 2:
+                    return len(new_input), new_input
+    return find_shortest_path(program, new_valid_programs)
+
+
+class Test2019Day15(unittest.TestCase):
+    # @parameterized.expand([
+    #     [],
+    # ])
+    def test_(self):
+        pass
+        # self.assertEqual(..., ...)
+
+
 if __name__ == '__main__':
     print(">>> Start Main 15:")
+    program = read_lines_as_list("data/15.txt")
+    i, path = find_shortest_path(program, [[]])
     print("Part 1):")
+    print(i)
     print("Part 2):")
     print("End Main 15<<<")
---
+++ 2021/Day04.py
@@ -0,0 +1,87 @@
+import unittest
+from parameterized import parameterized
+from typing import Dict, List, Tuple, Set
+
+from helper.file import load_file_split
+
+
+def parse_data(filepath: str) -> (List[int], List[List[int]]):
+    """load the random Numbers and boards from the file"""
+    parts = load_file_split(filepath, "\n\n")
+    numbers = [int(num) for num in parts[0].split(",")]
+    boards = []
+    for board in parts[1:]:
+        board_data = []
+        for line in board.split("\n"):
+            board_data += [int(line[i:i + 3].strip()) for i in range(0, len(line), 3)]
+        boards.append(board_data)
+    return numbers, boards
+
+
+def play_bingo(numbers: List[int], boards: List[List[int]]) -> int:
+    """play bingo, only horiz and vert lines count - return sum of not-chosen numbers times last digit"""
+    for number in numbers:
+        for board in boards:
+            try:
+                # mark the number on every board
+                idx: int = board.index(number)
+                board[idx] = None
+                # check changed boards for lines using idx
+                remainder = idx % 5
+                row_start = (idx - remainder)
+                # horizontal (neighbors) or vertical (every fifth)
+                if all(val is None for val in board[row_start:row_start + 5]) or \
+                        all(val is None for val in board[remainder::5]):
+                    return number * sum(val for val in board if val is not None)
+
+            except ValueError:
+                pass
+    raise Exception("No valid field found.")
+
+
+def last_board(numbers: List[int], boards: List[List[int]]) -> int:
+    """play every board until every board won at least once, return sum of the last board"""
+    for number in numbers:
+        remove_ids = []
+        for board_id, board in enumerate(boards):
+            try:
+                # mark the number on every board
+                idx: int = board.index(number)
+                board[idx] = None
+                # check changed boards for lines using idx
+                remainder = idx % 5
+                row_start = (idx - remainder)
+                # horizontal (neighbors) or vertical (every fifth)
+                if all(val is None for val in board[row_start:row_start + 5]) or \
+                        all(val is None for val in board[remainder::5]):
+                    # save id for removal
+                    remove_ids.append(board_id)
+                    # current board is final board -> worst choice
+                    if len(boards) - len(remove_ids) == 0:
+                        return number * sum(val for val in board if val is not None)
+            except ValueError:
+                pass
+        # remove boards from possible choices
+        boards = [boards[i] for i in range(len(boards)) if i not in remove_ids]
+    raise Exception("No valid field found.")
+
+
+
+class Test2021Day04(unittest.TestCase):
+
+    def test_basic_bingo(self):
+        num, boards = parse_data("data/04-test.txt")
+        self.assertEqual(play_bingo(num, boards), 4512)
+
+    def test_last_board(self):
+        num, boards = parse_data("data/04-test.txt")
+        self.assertEqual(last_board(num, boards), 1924)
+
+
+if __name__ == '__main__':
+    print(">>> Start Main 04:")
+    puzzle_input_numbers, puzzle_input_boards = parse_data("data/04.txt")
+    print("Part 1): ", play_bingo(puzzle_input_numbers, puzzle_input_boards))
+    puzzle_input_numbers, puzzle_input_boards = parse_data("data/04.txt")
+    print("Part 2): ", last_board(puzzle_input_numbers, puzzle_input_boards))
+    print("End Main 04<<<")
