337798ff70734edf77f3d78c3a5f023a0df7b797
Martin <bmmartint.stb@web.de>
2021-12-23 02:18:15 +0100

    more tuple helpers.
    WIP: Instead of using Union, should use TypeVar

--- helper/tuple.py
+++ helper/tuple.py
@@ -1,32 +1,46 @@
 import math
-from typing import Dict, List, Tuple, Set
+from typing import Dict, List, Tuple, Set, Union
+
+BaseVector = Tuple[int, ...]
 
 
-def tuple_add_tuple(a: Tuple[int, ...], b: Tuple[int, ...]):
+def tuple_add_tuple(a: BaseVector, b: BaseVector) -> BaseVector:
     """sum of two tuples"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
     return tuple(map(lambda i, j: i + j, a, b))
 
 
-def tuple_add_scalar(a: Tuple[int, ...], b: int):
+def tuple_add_scalar(a: BaseVector, b: Union[int, float]) -> BaseVector:
     """add constant to every val of tuple"""
     return tuple(map(lambda i: i + b, a))
 
 
-def tuple_mult_tuple(a: Tuple[int, ...], b: Tuple[int, ...]):
+def tuple_dot_tuple(a: BaseVector, b: BaseVector) -> Union[int, float]:
+    """scalar product between two equal length tuples"""
+    if len(a) != len(b):
+        raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
+    return sum(map(lambda i, j: i * j, a, b))
+
+
+def tuple_mult_tuple(a: BaseVector, b: BaseVector) -> BaseVector:
     """multiply values of two equal length tuples"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
     return tuple(map(lambda i, j: i * j, a, b))
 
 
-def tuple_mult_scalar(a: Tuple[int, ...], b: int):
+def tuple_mult_scalar(a: BaseVector, b: Union[int, float]) -> BaseVector:
     """multiply every value of tuple with scalar"""
     return tuple(map(lambda i: i * b, a))
 
 
-def euclidean_distance(a: Tuple[int, ...], b: Tuple[int, ...]) -> float:
+def tuple_mod_number(a: BaseVector, b: Union[int, float]) -> BaseVector:
+    """modulo of every value in tuple"""
+    return tuple(map(lambda i: i % b, a))
+
+
+def euclidean_distance(a: BaseVector, b: BaseVector) -> float:
     """calculate euclidean distance between two points"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
