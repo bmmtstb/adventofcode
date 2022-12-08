8ed706d0997cc7e4ca9c23fb0168f493fe367b68
Martin <bmmartint.stb@web.de>
2022-12-08 17:28:21 +0100

    2022 - Day 08 p1

---
+++ 2022/Day08.py
@@ -0,0 +1,112 @@
+import unittest
+from typing import Dict, List, Tuple, Set
+
+import numpy as np
+
+from helper.file import read_lines_as_list
+
+
+def get_visible_indices(tree_line: np.ndarray) -> List[int]:
+    """from a line of trees get the indices of every tree that is visible, always from start to end of list"""
+    visible = []
+    max_height = -1
+
+    for tree_idx, tree in enumerate(tree_line):
+        if tree > max_height:
+            visible.append(tree_idx)
+            max_height = tree
+    return visible
+
+
+def get_outside_visible_count(forest: np.ndarray) -> int:
+    """get the count of trees visible from outside"""
+    height, width = forest.shape
+    # add border
+    visible_indices: Set[Tuple[int, int]] = set(
+        [(0, i) for i in range(width)] +
+        [(height - 1, i) for i in range(width)] +
+        [(j, 0) for j in range(height)] +
+        [(j, width - 1) for j in range(height)]
+    )
+    # left to right and right to left
+    # does not have to analyze first nor last row, they have been added either way
+    for tree_line_id, tree_line in enumerate(forest[1:-1], start=1):
+        # l to r
+        visible_tree_indices = get_visible_indices(tree_line)
+        visible_indices.update(set((tree_line_id, tree_id) for tree_id in visible_tree_indices))
+        # r to l
+        visible_tree_indices = get_visible_indices(tree_line[::-1])
+        visible_indices.update(set((tree_line_id, width - 1 - tree_id) for tree_id in visible_tree_indices))
+
+    # top to bottom and bottom to top
+    # does not have to analyze first nor last row, they have been added either way
+    for tree_line_id in range(1, height - 1):
+        # top to bottom
+        visible_tree_indices = get_visible_indices(forest[:, tree_line_id])
+        visible_indices.update(set((tree_id, tree_line_id) for tree_id in visible_tree_indices))
+        # bottom to top
+        visible_tree_indices = get_visible_indices(forest[::-1, tree_line_id])
+        visible_indices.update(set((height - 1 - tree_id, tree_line_id) for tree_id in visible_tree_indices))
+
+    return len(visible_indices)
+
+
+class Test2021Day08(unittest.TestCase):
+    test_area_values = np.array([
+        [3, 0, 3, 7, 3],
+        [2, 5, 5, 1, 2],
+        [6, 5, 3, 3, 2],
+        [3, 3, 5, 4, 9],
+        [3, 5, 3, 9, 0],
+    ])
+    pyramid_trees = np.array([
+        [1, 2, 3, 2, 1],
+        [2, 3, 4, 3, 2],
+        [3, 4, 5, 4, 3],
+        [2, 3, 4, 3, 2],
+        [1, 2, 3, 2, 1],
+    ])
+
+    def test_load_data(self):
+        # dummy check
+        self.assertTrue(
+            np.alltrue(
+                np.array(read_lines_as_list(filepath="data/08-test.txt", instance_type=int, split="every")) == \
+                self.test_area_values
+            ))
+
+    def test_get_visible_indices(self):
+        for tree_list, visible_trees in [
+            (self.test_area_values[0], [0, 3]),
+            (self.test_area_values[1], [0, 1]),
+            (self.test_area_values[2], [0]),
+            (self.test_area_values[3], [0, 2, 4]),
+            (self.test_area_values[4], [0, 1, 3]),
+            (self.test_area_values[0, ::-1], [0, 1]),
+            (self.test_area_values[1, ::-1], [0, 2]),
+            (self.test_area_values[2, ::-1], [0, 1, 3, 4]),
+            (self.test_area_values[3, ::-1], [0]),
+            (self.test_area_values[4, ::-1], [0, 1]),
+            ([1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8]),
+            ([1, 1, 1, 1], [0]),
+        ]:
+            with self.subTest(msg=f'tree list: {tree_list}'):
+                self.assertEqual(get_visible_indices(tree_list), visible_trees)
+
+    def test_get_outside_visible_count(self):
+        for area, count in [
+            (self.test_area_values, 21),
+            (np.rot90(self.test_area_values), 21),
+            (self.pyramid_trees, 25),
+            (np.array(read_lines_as_list(filepath="data/08.txt", instance_type=int, split="every")), 1733),
+        ]:
+            with self.subTest(msg=f'count: {count}'):
+                self.assertEqual(get_outside_visible_count(area), count)
+
+
+if __name__ == '__main__':
+    print(">>> Start Main 08:")
+    puzzle_input = np.array(read_lines_as_list(filepath="data/08.txt", instance_type=int, split="every"))
+    print("Part 1): ", get_outside_visible_count(puzzle_input))
+    print("Part 2): ")
+    print("End Main 08<<<")
