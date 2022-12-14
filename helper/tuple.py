816d61826266430504cf2e906fde947a7f26ad87
Martin <bmmartint.stb@web.de>
2022-12-14 19:03:01 +0100

    streamlined usage of tuple helpers

--- helper/tuple.py
+++ helper/tuple.py
@@ -1,54 +1,55 @@
 import math
-from typing import Tuple, Union
+from typing import Tuple, TypeVar, Union
 
 Number = Union[int, float]
 BaseVector = Tuple[Number, ...]
+BaseVectorLength = TypeVar("BaseVectorLength", bound=BaseVector)
 
 
-def tuple_add_tuple(a: BaseVector, b: BaseVector) -> BaseVector:
+def tuple_add_tuple(a: BaseVectorLength, b: BaseVectorLength) -> BaseVectorLength:
     """sum of two tuples"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
     return tuple(map(lambda i, j: i + j, a, b))
 
 
-def tuple_subtract_tuple(a: BaseVector, b: BaseVector) -> BaseVector:
+def tuple_subtract_tuple(a: BaseVectorLength, b: BaseVectorLength) -> BaseVectorLength:
     """difference of first minus second tuple"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
     return tuple(map(lambda i, j: i - j, a, b))
 
 
-def tuple_add_scalar(a: BaseVector, b: Number) -> BaseVector:
+def tuple_add_scalar(a: BaseVectorLength, b: Number) -> BaseVectorLength:
     """add constant to every val of tuple"""
     return tuple(map(lambda i: i + b, a))
 
 
-def tuple_dot_tuple(a: BaseVector, b: BaseVector) -> Number:
+def tuple_dot_tuple(a: BaseVectorLength, b: BaseVectorLength) -> Number:
     """scalar product between two equal length tuples"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
     return sum(map(lambda i, j: i * j, a, b))
 
 
-def tuple_mult_tuple(a: BaseVector, b: BaseVector) -> BaseVector:
+def tuple_mult_tuple(a: BaseVectorLength, b: BaseVectorLength) -> BaseVectorLength:
     """multiply values of two equal length tuples"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
     return tuple(map(lambda i, j: i * j, a, b))
 
 
-def tuple_mult_scalar(a: BaseVector, b: Number) -> BaseVector:
+def tuple_mult_scalar(a: BaseVectorLength, b: Number) -> BaseVectorLength:
     """multiply every value of tuple with scalar"""
     return tuple(map(lambda i: i * b, a))
 
 
-def tuple_mod_number(a: BaseVector, b: Number) -> BaseVector:
+def tuple_mod_number(a: BaseVectorLength, b: Number) -> BaseVectorLength:
     """modulo of every value in tuple"""
     return tuple(map(lambda i: i % b, a))
 
 
-def euclidean_distance(a: BaseVector, b: BaseVector) -> float:
+def euclidean_distance(a: BaseVectorLength, b: BaseVectorLength) -> float:
     """calculate euclidean distance between two points"""
     if len(a) != len(b):
         raise ValueError("Tuples should have same length. {} != {}".format(len(a), len(b)))
