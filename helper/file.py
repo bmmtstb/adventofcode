852a900087fbb6b8e5045e99907d85af399a008f
Martin <1500595+bmmtstb@users.noreply.github.com>
2024-12-02 01:18:14 +0100

    fix linting issues with helper files
    Signed-off-by: Martin <1500595+bmmtstb@users.noreply.github.com>

--- helper/file.py
+++ helper/file.py
@@ -1,10 +1,18 @@
+"""
+General file handling functions.
+
+Most day solutions require reading some sort of list from a file.
+"""
+
 from typing import List, Union, Callable
 
 
 def load_file_and_split(
     filepath: str, separator: str = "\n", instance_type: type = str
 ) -> List[Union[int, str, float]]:
-    """read a file and split it at every occurrence of separator, the separated instances will be cast to instanceType"""
+    """Read a file and split it at every occurrence of separator.
+    The separated instances will be cast to instanceType.
+    """
     with open(filepath, "r", encoding="utf-8") as file:
         lines = [instance_type(val) for val in file.read().split(separator)]
     return lines
@@ -13,7 +21,10 @@ def load_file_and_split(
 def read_lines_as_list(
     filepath, instance_type: Union[type, Callable] = str, split: str = None
 ):
-    """reads the file at filepath, splits "every" line into substrings if provided, casts to type"""
+    """Reads the file at filepath.
+    Splits "every" line into substrings if provided.
+    Additionally, casts the value(s) of all (possibly split lines) to the given type.
+    """
     data = []
     with open(filepath, "r", encoding="utf-8") as file:
         for line in file.readlines():
